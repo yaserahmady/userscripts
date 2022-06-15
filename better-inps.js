72db0813dd0e330d4f68be3742be4aa48e01da18
Yaser Ahmady <y@yaserahmady.com>
2022-06-15 15:59:40 +0200

    Add Better INPS

---
+++ better-inps.js
@@ -0,0 +1,72 @@
+(function () {
+  ("use strict");
+
+  addScript("https://cdn.tailwindcss.com");
+  addScript(
+    "https://cdn.jsdelivr.net/npm/@marcreichel/alpine-autosize@latest/dist/alpine-autosize.min.js"
+  );
+  addScript("https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js", true);
+
+  document.querySelectorAll("textarea").forEach((textarea) => {
+    // Edit parent
+    let td = textarea.parentNode;
+    td.setAttribute("x-data", "{ content: '' }");
+    td.classList.add("flex", "space-x-2", "items-center");
+
+    // Add character count element
+    let characterCountEl = document.createElement("div");
+    let characterCountMarkup = `
+    <p>
+        <span x-count="content"></span>/<span>1000</span>
+    </p>
+    `;
+    characterCountEl.innerHTML = characterCountMarkup;
+    textarea.insertAdjacentElement("afterend", characterCountEl);
+
+    // Edit textarea
+    textarea.setAttribute("x-model", "content");
+    textarea.setAttribute("x-autosize", "");
+    textarea.classList.add("font-normal", "font-mono", "text-[16px]");
+  });
+
+  // Resize tables
+  document.querySelectorAll("table").forEach((table) => {
+    table.setAttribute("width", "80%");
+    table.classList.add("table-auto");
+  });
+})();
+
+function addScript(url, defer = false) {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  if (defer) script.setAttribute("defer", "");
+
+  document.head.appendChild(script);
+}
+
+//
+// Character count plugin
+//
+
+const count = () => {
+  Alpine.directive(
+    "count",
+    (el, { expression, modifiers }, { evaluateLater, effect }) => {
+      let maxLength = modifiers[0] || false;
+      let getInputValue = evaluateLater(expression);
+
+      effect(() => {
+        getInputValue((string) => {
+          let stringLength = string.length;
+
+          el.innerText = maxLength ? maxLength - stringLength : stringLength;
+        });
+      });
+    }
+  );
+};
+
+document.addEventListener("alpine:init", () => {
+  window.Alpine.plugin(count);
+});
